a992d86027f98aa11039e067298229cc0f2c1467
PedrOrtizM <pedrortizm18_@hotmail.com>
2021-11-04 14:27:38 -0500

    linter fix

--- test/userRegistration.spec.js
+++ test/userRegistration.spec.js
@@ -9,38 +9,31 @@ const userMock = {
   password: 'Pass12341234'
 };
 
-describe("POST /users", () => {
-
-  test("It should respond user name with status 201", async () => {
-
+describe('POST /users', () => {
+  test('It should respond user name with status 201', async () => {
     const response = await request(app).post('/users').send(userMock);
-
     expect(response.body.name).toBe(userMock.name);
     expect(response.statusCode).toBe(201);
   });
 
-  test("It should respond error when email is duplicate", async () => {
-
+  test('It should respond error when email is duplicate', async () => {
     await request(app).post('/users').send(userMock);
     const response = await request(app).post('/users').send(userMock);
-
     expect(response.body.internal_code).toBe(DATABASE_ERROR);
     expect(response.statusCode).toBe(503);
   });
 
-  test("It should respond error when password is invalid", async () => {
+  test('It should respond error when password is invalid', async () => {
     const userInvalid = { ...userMock };
     userInvalid.password = 'invalid';
-
     const response = await request(app).post('/users').send(userInvalid);
     expect(response.body[0].keyword).toBe('minLength');
     expect(response.statusCode).toBe(400);
   });
 
-  test("It should respond error when email is not present", async () => {
+  test('It should respond error when email is not present', async () => {
     const userInvalid = { ...userMock };
     userInvalid.email = undefined;
-
     const response = await request(app).post('/users').send(userInvalid);
     expect(response.body[0].keyword).toBe('required');
     expect(response.statusCode).toBe(400);
