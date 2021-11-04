e8038ec8c27a412d8be1ddfe83216c61c352002e
PedrOrtizM <pedrortizm18_@hotmail.com>
2021-11-04 16:18:32 -0500

    custom errors

--- app/errors.js
+++ app/errors.js
@@ -8,3 +8,6 @@ exports.databaseError = message => internalError(message, exports.DATABASE_ERROR
 
 exports.DEFAULT_ERROR = 'default_error';
 exports.defaultError = message => internalError(message, exports.DEFAULT_ERROR);
+
+exports.SCHEMA_ERROR = 'schema_error';
+exports.schemaError = message => internalError(message, exports.SCHEMA_ERROR);
--- app/middlewares/validateSchema.js
+++ app/middlewares/validateSchema.js
@@ -1,10 +1,13 @@
 const Ajv = require('ajv');
-const addFormats = require('ajv-formats')
+const addFormats = require('ajv-formats');
+const { schemaError } = require('../errors');
 
 const ajv = new Ajv()
 addFormats(ajv);
 
 module.exports.validateSchema = schema => (req, res, next) => {
   const validate = ajv.compile(schema);
-  validate(req.body) ? next() : res.status(400).send(validate.errors);
-};
+  if (validate(req.body)) return next();
+
+  next(schemaError(validate.errors));
+}
