2e94d72c79477c96e33350e5bb7e8bdc663c9f86
PedrOrtizM <pedrortizm18_@hotmail.com>
2021-11-04 14:50:19 -0500

    fix lint

--- test/userRegistration.spec.js
+++ test/userRegistration.spec.js
@@ -11,14 +11,20 @@ const userMock = {
 
 describe('POST /users', () => {
   test('It should respond user name with status 201', async () => {
-    const response = await request(app).post('/users').send(userMock);
+    const response = await request(app)
+      .post('/users')
+      .send(userMock);
     expect(response.body.name).toBe(userMock.name);
     expect(response.statusCode).toBe(201);
   });
 
   test('It should respond error when email is duplicate', async () => {
-    await request(app).post('/users').send(userMock);
-    const response = await request(app).post('/users').send(userMock);
+    await request(app)
+      .post('/users')
+      .send(userMock);
+    const response = await request(app)
+      .post('/users')
+      .send(userMock);
     expect(response.body.internal_code).toBe(DATABASE_ERROR);
     expect(response.statusCode).toBe(503);
   });
@@ -26,7 +32,9 @@ describe('POST /users', () => {
   test('It should respond error when password is invalid', async () => {
     const userInvalid = { ...userMock };
     userInvalid.password = 'invalid';
-    const response = await request(app).post('/users').send(userInvalid);
+    const response = await request(app)
+      .post('/users')
+      .send(userInvalid);
     expect(response.body[0].keyword).toBe('minLength');
     expect(response.statusCode).toBe(400);
   });
@@ -34,7 +42,9 @@ describe('POST /users', () => {
   test('It should respond error when email is not present', async () => {
     const userInvalid = { ...userMock };
     userInvalid.email = undefined;
-    const response = await request(app).post('/users').send(userInvalid);
+    const response = await request(app)
+      .post('/users')
+      .send(userInvalid);
     expect(response.body[0].keyword).toBe('required');
     expect(response.statusCode).toBe(400);
   });
