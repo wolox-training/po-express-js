ee0ab44ea4e6fdfc896872c932e5c14a31dc3b02
PedrOrtizM <pedrortizm18_@hotmail.com>
2021-11-05 13:30:49 -0500

    post user documentation

--- documentation/paths/user.js
+++ documentation/paths/user.js
@@ -1,33 +1,5 @@
 module.exports = {
   '/users': {
-    get: {
-      tags: ['CRUD operations'],
-      description: 'Get users',
-      operationId: 'getUsers',
-      parameters: [
-        {
-          name: 'page',
-          in: 'query',
-          schema: {
-            type: 'integer',
-            default: 1
-          },
-          required: false
-        }
-      ],
-      responses: {
-        200: {
-          description: 'Users were obtained',
-          content: {
-            'application/json': {
-              schema: {
-                $ref: '#/components/schemas/Users'
-              }
-            }
-          }
-        }
-      }
-    },
     post: {
       tags: ['CRUD operations'],
       description: 'Create user',
@@ -44,19 +16,60 @@ module.exports = {
         required: true
       },
       responses: {
-        200: {
-          description: 'New user was created'
+        201: {
+          description: 'New user was created',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/User'
+              }
+            }
+          }
         },
-        400: {
-          description: 'Invalid parameters',
+        409: {
+          description: 'Duplicate email error',
           content: {
             'application/json': {
               schema: {
                 $ref: '#/components/schemas/Error'
               },
               example: {
-                message: 'User´s email already exists',
-                internal_code: 'invalid_parameters'
+                message: 'duplicate key violates unique constraint "users_email_key"',
+                internal_code: 'email_duplicate_error'
+              }
+            }
+          }
+        },
+        422: {
+          description: 'Schema validation error',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/Error'
+              },
+              example: {
+                message: 'must have required property lastName',
+                internal_code: 'schema_error'
+              }
+            }
+          }
+        },
+        500: {
+          description: 'Default error',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/Error'
+              }
+            }
+          }
+        },
+        503: {
+          description: 'Database error',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/Error'
               }
             }
           }
