a0703f3433ebbed0a54e2a7d3d795d347009690e
PedrOrtizM <pedrortizm18_@hotmail.com>
2021-11-09 16:14:17 -0500

    rename functions

--- app/helpers/jwt.js
+++ app/helpers/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
 const { secret } = require('../../config').common.session;
 
-module.exports.getToken = payload => jwt.sign(payload, secret);
+module.exports.createToken = payload => jwt.sign(payload, secret);
 module.exports.verifyToken = token => jwt.verify(token, secret);
