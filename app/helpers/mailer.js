de706f04cb1a12e4365382c2264043a8815437a3
Pedro Ortiz M <pedrortizm18_@hotmail.com>
2021-11-29 09:11:34 -0500

    mailer as promise

--- app/helpers/mailer.js
+++ app/helpers/mailer.js
@@ -5,9 +5,9 @@ const { email: user, pass, service } = require('../../config').common.mailer;
 module.exports.sendMail = options => {
   try {
     const transporter = nodemailer.createTransport({ service, auth: { user, pass } });
-    transporter.sendMail({ from: user, ...options }, (error, info) => {
-      error ? logger.error(error) : logger.info('Email sent ' + info.response);
-    });
+    transporter.sendMail({ from: user, ...options })
+      .then(info => logger.info('Email sent ' + info.response))
+      .catch(error => logger.error(error));
   } catch (error) {
     logger.error(error);
   }
